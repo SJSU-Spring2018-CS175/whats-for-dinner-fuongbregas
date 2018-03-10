c6bc3ddc5696bd881d73634413be3f9480517c46
fuongbregas <fuongbregas@icloud.com>
2018-03-10 11:09:23 -0800

    Revert "Revert "Finally fixed""
    This reverts commit ffa49fef91e4fb90e9ce01acfb75816757fe429a.

--- app/src/main/java/edu/sjsu/fuong/whatsfordinner/PortraitFrag.java
+++ app/src/main/java/edu/sjsu/fuong/whatsfordinner/PortraitFrag.java
@@ -3,21 +3,26 @@ package edu.sjsu.fuong.whatsfordinner;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.FragmentTransaction;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PortraitFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
-public class PortraitFrag extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+public class PortraitFrag extends Fragment  {
+
+    private ListView listViewPortrait;
+
+    public ArrayList<ArrayList<String>> allDishes;
+    public ArrayList<String> dishNames;
 
     public PortraitFrag() {
         // Required empty public constructor
@@ -27,46 +32,39 @@ public class PortraitFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        dishNames = new ArrayList<>();
+
+        if(args == null){
+            //Toast.makeText(PortraitFrag.this.getActivity(), "Args is Empty", Toast.LENGTH_LONG).show();
+        }else{
+            // Get ArrayList from RecipeAct
+            allDishes = (ArrayList<ArrayList<String>>) args.getSerializable("allDishes");
+            System.out.println("Length of allDishes " + allDishes.size());
+            // Get all the string at index 0, which are names of dishes
+
+            for(int i = 0; i < allDishes.size(); i++){
+                System.out.println("Dish Name: " + allDishes.get(i).get(0));
+                dishNames.add(allDishes.get(i).get(0));
+            }
+            System.out.println("Length of dishNames " + dishNames.size());
+
+
+        }
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_portrait, container, false);
+        View view = inflater.inflate(R.layout.fragment_portrait, container, false);
+        ArrayAdapter portraitAdapter = new ArrayAdapter(PortraitFrag.this.getActivity(), android.R.layout.simple_expandable_list_item_1, dishNames);
+        listViewPortrait = (ListView) view.findViewById(R.id.listViewPortrait);
+        listViewPortrait.setAdapter(portraitAdapter);
+
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
+
 }
--- app/src/main/java/edu/sjsu/fuong/whatsfordinner/Welcome.java
+++ app/src/main/java/edu/sjsu/fuong/whatsfordinner/Welcome.java
@@ -79,7 +79,10 @@ public class Welcome extends AppCompatActivity {
         receipeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent recipeAct = new Intent(Welcome.this, RecipeActivity.class);
+                recipeAct.putStringArrayListExtra("ingredientList", (ArrayList<String>) ingredient);
+                recipeAct.putExtra("dishList", (ArrayList<ArrayList<String>>) allDishes);
+                startActivity(recipeAct);
             }
         });
     }
